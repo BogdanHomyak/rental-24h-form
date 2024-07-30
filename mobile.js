ae021dba30619e3e090f4766e002849195b07e0d
BogdanHomyak <homa1406c@gmail.com>
2024-07-31 01:22:28 +0300

    comit

--- mobile.js
+++ mobile.js
@@ -3,24 +3,34 @@ document.addEventListener('DOMContentLoaded', function() {
         const formContainer = document.querySelector('.search-container');
         const expandableElements = document.querySelectorAll('.expandable');
         const removeIcon = document.getElementById('remove-expanded-icon');
+        const returnLocationWrapper = document.getElementById('returnLocationWrapper')
+        const promoCodeField = document.querySelector('.promo-code');
 
         expandableElements.forEach(element => {
-            element.addEventListener('focus', function () {
-                formContainer.classList.add('expanded');
-                document.querySelectorAll('.return-location, .data-picker-container, .residence-and-age, .check-box, .input-wrapper, .submit-btn')
-                    .forEach(el => el.classList.add('expanded'));
-                removeIcon.style.display = 'inline';
+            element.addEventListener('input', function () {
+                if (element.value.trim() !== "") {
+                    formContainer.classList.add('expanded');
+                    document.querySelectorAll('.return-location, .data-picker-container, .residence-and-age, .check-box, .input-wrapper, .submit-btn, .promo-code, #returnLocationWrapper')
+                        .forEach(el => el.classList.add('expanded'));
+                    removeIcon.style.display = 'inline';
+                } else {
+                    formContainer.classList.remove('expanded');
+                    document.querySelectorAll('.return-location, .data-picker-container, .residence-and-age, .check-box, .input-wrapper, .submit-btn')
+                        .forEach(el => el.classList.remove('expanded'));
+                    removeIcon.style.display = 'none';
+                }
             });
-        })
+        });
 
 
         removeIcon.addEventListener('click', function () {
-
             formContainer.classList.remove('expanded');
             document.querySelectorAll('.return-location, .data-picker-container, .residence-and-age, .check-box, .input-wrapper, .submit-btn')
                 .forEach(el => el.classList.remove('expanded'));
-
             removeIcon.style.display = 'none';
+            returnLocationWrapper.classList.add('hidden')
+            promoCodeField.classList.remove('active');
+
         });
     }
 });
